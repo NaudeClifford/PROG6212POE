0d9d3154064f4aa60a11643988216a6bae49fa3f
Naude-Clifford <st10258513@imconnect.edu.za>
2025-10-21 13:24:09 +0200

    Commit 6
    Had improved the UI to be more pleasant and less boring. changed the home page, privacy page and the nav link to be more user friendly and easier to read.

--- PROG6212POE/obj/Debug/net9.0/PROG6212POE.AssemblyInfo.cs
+++ PROG6212POE/obj/Debug/net9.0/PROG6212POE.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6212POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538c181139ea7cb0b007c5bb87eb7db655e49b81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccfd92245e55fcad6b9deca9e6200081a391437")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6212POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6212POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
